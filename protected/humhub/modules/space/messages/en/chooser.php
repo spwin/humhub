a0c659fcc8393fe34ca23534dad2f2f4089d6f7f
Stan Mark <stanislav.markevic@gmail.com>
2022-03-06 15:34:57 +1100

    update gitignore and rename Spaces to Cases

---
+++ protected/humhub/modules/space/messages/en/chooser.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'Search' => '',
+    'Add Space' => 'Add Case',
+    'Create new space' => 'Create new case',
+    'My spaces' => 'My cases',
+    'No member or following spaces found.' => 'No member or following cases found.',
+    'No result found for the given filter.' => '',
+    'No spaces found for the given query' => 'No cases found for the given query',
+    'Search for spaces' => 'Search for cases',
+    'Select {n,plural,=1{space} other{spaces}}' => '',
+    'This field only allows a maximum of {n,plural,=1{# space} other{# spaces}}' => '',
+    'This space is archived' => 'This case is archived',
+    'To search for other spaces, type at least {count} characters.' => 'To search for other cases, type at least {count} characters.',
+    'You are following this space' => 'You are following this case',
+    '{n,plural,=1{# new entry} other{# new entries}} since your last visit' => '',
+];
---
+++ protected/humhub/modules/space/messages/en/permissions.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Allows the user to create private content' => '',
+    'Allows the user to create public content' => '',
+    'Allows the user to invite new members to the space' => '',
+    'Can Access \'Spaces\'' => 'Can Access \'Cases\'',
+    'Can access the \'Spaces\' section.' => 'Can access the \'Cases\' section.',
+    'Can create Spaces visible to all members.' => 'Can create Cases visible to all members.',
+    'Can create hidden (private) Spaces.' => 'Can create hidden (private) Cases.',
+    'Create Private Spaces' => 'Create Private Cases',
+    'Create Public Spaces' => 'Create Public Cases',
+    'Create private content' => '',
+    'Create public content' => '',
+    'Invite users' => '',
+];
